fd1780beb7eae369ad23fc7e205a766585840666
srikiran <66249083+skmld@users.noreply.github.com>
2021-07-16 15:04:13 +0530

    Update without_function_import_csv.py

--- without_function_import_csv.py
+++ without_function_import_csv.py
@@ -1,9 +1,9 @@
 import csv
-with open('../Data/example.csv', 'a', newline='') as csvfile:
+with open('../Data/csvexample.csv', 'a', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
-            writer.writerow(['1-2-2019',7,8,'ram'])
-            writer.writerow(['2-4-2019',10,2,'priyanka'])
-            writer.writerow(['4-4-2019',3,1,'manashi'])
+            writer.writerow(['1-2-2020',7,8,'ramana'])
+            writer.writerow(['2-2-2020',10,2,'priya'])
+            writer.writerow(['4-3-2020',3,1,'manasa'])
 
 
 
